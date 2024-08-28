413295d9ec18d99ba8652c7e19ffc3b4b0d2a68a
Christopher Woods <chryswoods@hey.com>
2024-08-28 17:07:08 +0100

    Added the client program, which will be the part that connects Python to the other agents, from where commands are issued and responses received

---
+++ client/src/main.rs
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: © 2024 Christopher Woods <Christopher.Woods@bristol.ac.uk>
+// SPDX-License-Identifier: MIT
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // construct a subscriber that prints formatted traces to stdout
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let defaults = paddington::Defaults::new(
+        Some("client".to_string()),
+        Some(
+            "client.toml"
+                .parse()
+                .expect("Could not parse default config file."),
+        ),
+        Some("ws://localhost:8041".to_string()),
+        Some("127.0.0.1".to_string()),
+        Some(8041),
+    );
+
+    templemeads::agent::run(defaults).await?;
+
+    Ok(())
+}
